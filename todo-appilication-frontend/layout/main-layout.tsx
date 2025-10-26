f61b150bee12f8a0a47e76f6b1ed907771b5c0f9
Lakshan Chamara <87594213+lakshan-cd@users.noreply.github.com>
2025-10-26 18:18:57 +0530

    Integrate Redux store into MainLayout component: wrap children with Provider for state management and ensure proper context for child components.

--- todo-appilication-frontend/layout/main-layout.tsx
+++ todo-appilication-frontend/layout/main-layout.tsx
@@ -2,6 +2,8 @@
 import HeaderComponent from "@/components/header/Header";
 import SnackbarProvider from "@/components/snackbar";
 import Providers from "@/provider/providers";
+import { store } from "@/redux/store";
+import { Provider } from "react-redux";
 
 export default function MainLayout({
     children,
@@ -12,10 +14,12 @@ export default function MainLayout({
         <>
         <Providers>
             <SnackbarProvider>
+            <Provider store={store} >
                 <HeaderComponent/>
                 <div className="min-h-screen">
                     {children}
                 </div>
+            </Provider>
             </SnackbarProvider>
         </Providers>
         </>
