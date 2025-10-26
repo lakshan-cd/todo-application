05ec901ac6c11d9489b909fd4f2417259213f045
Lakshan Chamara <87594213+lakshan-cd@users.noreply.github.com>
2025-10-26 19:44:12 +0530

    Remove unused Lombok annotation from ResponseHolder

--- todo-application-backend/src/main/java/com/lakshancd/todo_application_backend/payload/response/support/ResponseHolder.java
+++ todo-application-backend/src/main/java/com/lakshancd/todo_application_backend/payload/response/support/ResponseHolder.java
@@ -1,6 +1,5 @@
 package com.lakshancd.todo_application_backend.payload.response.support;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
