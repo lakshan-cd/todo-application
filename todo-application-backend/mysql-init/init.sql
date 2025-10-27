080e6cd1ea1295a61a190cdc835194c5837b8f52
Lakshan Chamara <87594213+lakshan-cd@users.noreply.github.com>
2025-10-27 09:54:12 +0530

    Add task table structure to MySQL initialization script, including fields for task management and foreign key constraint to statusdetails.

--- todo-application-backend/mysql-init/init.sql
+++ todo-application-backend/mysql-init/init.sql
@@ -15,3 +15,27 @@ CREATE TABLE statusdetails (
 INSERT INTO statusdetails (status, description) VALUES
 (10, 'Pending'),
 (20, 'Completed');
+
+-- --------------------------------------------------------
+-- Table structure for table `task`
+-- --------------------------------------------------------
+
+CREATE TABLE `task` (
+                        `uid` INT NOT NULL AUTO_INCREMENT,
+                        `created_by` VARCHAR(255) DEFAULT NULL,
+                        `created_date` DATETIME(6) DEFAULT NULL,
+                        `modified_by` VARCHAR(255) DEFAULT NULL,
+                        `modified_date` DATETIME(6) DEFAULT NULL,
+                        `title` VARCHAR(255) DEFAULT NULL,
+                        `description` VARCHAR(255) DEFAULT NULL,
+                        `status_id` INT DEFAULT NULL,
+                        `is_active` BIT(1) DEFAULT NULL,
+                        PRIMARY KEY (`uid`),
+                        KEY `FK_task_status` (`status_id`),
+                        CONSTRAINT `FK_task_status` FOREIGN KEY (`status_id`) REFERENCES `statusdetails` (`status`)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_0900_ai_ci;
+
+
+
